c313d40547deede2061ade1a2462fb8a59cd0274
panyanxin <1205307815@qq.com>
2021-02-17 23:59:32 +0800

    项目的基本配置

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import React, { PureComponent } from 'react'
+import React, { memo } from 'react'
 
-export default class App extends PureComponent {
-  render() {
-    return (
-      <div>
-        hello world
-      </div>
-    )
-  }
-}
+// export default class App extends PureComponent {
+//   render() {
+//     return (
+//       <div>
+//         hello world
+//       </div>
+//     )
+//   }
+// }
+
+export default memo(function App() {
+  return (
+    <div>
+      123
+    </div>
+  )
+})
